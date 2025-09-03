2e83099bdd9923a58fa3cc89fb31c46a7ec80174
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-09-03 20:41:58 +0200

    completato ultimo esercizio

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,25 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
+function contaVocali(parola) {
+  const vocali = "aeiou";
+  let contatore = []; // qui mettiamo le vocali trovate
+
+  for (let i = 0; i < parola.length; i++) {
+    const carattere = parola[i].toLowerCase();
+    if (vocali.includes(carattere)) {
+      contatore.push(parola[i]); // aggiungiamo il carattere originale
+    }
+  }
+
+  return contatore; // restituiamo l'array di vocali trovate
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const risultato = contaVocali(word);
 
-
+console.log("Vocali trovate:", risultato); 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
